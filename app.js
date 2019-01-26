e375fc9a7426eb0812fa01f788775e56587907fe
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2019-01-26 16:21:16 +0100

    refactoring js part 1

--- app.js
+++ app.js
@@ -1,74 +1,49 @@
-var currentTab = 0; // Current tab is set to be the first tab (0)
+let popupFormHtml = `
+ <div id="modal" class="overlay"> <div class="popup"> <a class="close" href="#">&times;</a> <div class="popup-content"> <form id="surveyForm"> <h2>Dummy Survey</h2> <p class="intro">Welcome to the our dummy survey!</p><p class="message"></p><div class="tabs-container"> <div class="tab"> <div class="form-field"> <label for="userName">Name:</label> <input class="u-full-width user-input" type="text" placeholder="Your name" id="userName" name="Name" required> </div><div class="form-field"> <label for="userEmail">E-mail:</label> <input class="u-full-width user-input" type="email" placeholder="Your e-mail address" id="userEmail" name="Email" required> </div></div><div class="tab"> <div class="form-field"> <label for="userAge">Age:</label> <select class="u-full-width user-input" id="userAge" name="Age" required> <option value="">--Select Age--</option> </select> </div><div class="form-field"> <label for="userAboutMe">About Me:</label> <textarea class="u-full-width user-input" placeholder="About me..." id="userAboutMe" name="AboutMe" required></textarea> </div></div><div class="tab"> <div class="form-field"> <label for="userAddress">Adress:</label> <input class="u-full-width user-input" type="text" placeholder="Address" id="userAddress" name="Address"> </div><div class="form-field"> <label>Gender:</label> <label> <input type="radio" name="Gender" value="male" class="user-input"> <span class="label-body">Male</span> </label> <label> <input type="radio" name="Gender" value="female" class="user-input"> <span class="label-body">Female</span> </label> </div></div><div class="tab"> <div class="form-field"> <label for="userFavouriteBook">Favourite book:</label> <input class="u-full-width user-input" type="text" placeholder="Favourite book" id="userFavouriteBook" name="FavouriteBook"> </div><div class="form-field checkboxex"> <label>Favourite colors:</label> <label> <input type="checkbox" class="user-input" name="FavouriteColor" value="Red"> <span class="label-body">Red</span> </label> <label> <input type="checkbox" class="user-input" name="FavouriteColor" value="Aqua"> <span class="label-body">Aqua</span> </label> <label> <input type="checkbox" class="user-input" name="FavouriteColor" value="Blue"> <span class="label-body">Blue</span> </label> <label> <input type="checkbox" class="user-input" name="FavouriteColor" value="Crimson"> <span class="label-body">Crimson</span> </label> </div></div></div><div class="buttons-section"> <div class="buttons-container"> <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button> <button type="button" class="button-primary" id="nextBtn" onclick="nextPrev(1)">Next</button> </div></div><div class="steps-container"></div></form> </div></div></div>
+`;
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  //placeholder for creating a form and injectin it
-  populateAgeDropdown();
-  generateStepsIndicators();
-
-
+createForm();
+populateAgeDropdown();
+generateStepsIndicators();
+let currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
-  readFromLocalStorage();
-
-});
-
-
-function generateStepsIndicators() {
-  const steps = document.querySelectorAll('#modal .tab');
-  const stepsContainer = document.querySelector('.steps-container');
-
-  for(let i = 0; i < steps.length; i++) {
-    let singeStepIndicator = document.createElement('span');
-    singeStepIndicator.classList = "step";
-
-    stepsContainer.appendChild(singeStepIndicator)
-    console.log('step')
-  }
-
-}
-
-
-function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
-  var i, x = document.getElementsByClassName("step");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-  }
-  //... and adds the "active" class to the current step:
-  x[n].className += " active";
-}
-
- 
-
 //Define UI variables
-
-
-
-// function loadEventListeners() {
-//   //
-// }
 const modal = document.querySelector('#modal');
 const closeBtn = document.querySelector('.close');
 
-closeBtn.addEventListener('click', ()=> {
-  modal.classList.toggle('modal-active');
-})
+document.addEventListener("DOMContentLoaded", function() { 
+  setTimeout(() => {
+    modal.classList.toggle('modal-active');
+  },2000);
 
-setTimeout(() => {
-  modal.classList.toggle('modal-active');
-}) //dont forget to put 2 secs here
+  closeBtn.addEventListener('click', ()=> {
+    modal.classList.toggle('modal-active');
+  })
 
+  readFromLocalStorage();
+  // saveToLocalStorage();
+  
+});
 
 const user = {
-  Name: "HipHop",
-  Email: "hiphop@o2.pl",
-  Age: "10",
-  AboutMe: "Kiss me",
-  Address: "Moja ulica",
-  Gender: "Male",
-  FavouriteBook: "Wladca pierscieni",
-  FavouriteColor: ['Red','Crimson']
+  Name: "",
+  Email: "",
+  Age: "",
+  AboutMe: "",
+  Address: "",
+  Gender: "",
+  FavouriteBook: "",
+  FavouriteColor: []
 }
+
+function createForm() {
+  let body = document.querySelector('body');
+  let formContainer = document.createElement('div');
+  formContainer.innerHTML = popupFormHtml;
+  body.appendChild(formContainer);
+}
+
 function populateAgeDropdown() {
   var ageDropdown = document.getElementById('userAge');
   const startAge = 10;
@@ -79,12 +54,166 @@ function populateAgeDropdown() {
       theOption.value = i;
       ageDropdown.options[i - startAge+1] = theOption;
   }
-  console.log('populated the dropdown');
+
+}
+
+function generateStepsIndicators() {
+  const steps = document.querySelectorAll('#modal .tab');
+  const stepsContainer = document.querySelector('.steps-container');
+
+  for(let i = 0; i < steps.length; i++) {
+    let singeStepIndicator = document.createElement('span');
+    singeStepIndicator.classList = "step";
+    stepsContainer.appendChild(singeStepIndicator)
+  }
+
+}
+
+function showTab(n) {
+  // Display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].classList.add('tab-active');
+
+  // fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  
+  fixStepIndicator(n)
+
+}
+
+function fixStepIndicator(n) {
+  let steps = document.getElementsByClassName("step");
+  for (let i = 0; i < steps.length; i++) {
+    steps[i].className = steps[i].className.replace(" active", "");
+  }
+  steps[n].className += " active";
 }
 
 
-saveToLocalStorageRad();
-function saveToLocalStorageRad() {
+function nextPrev(n) {
+  var collectionOfTabs = document.getElementsByClassName("tab");
+
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+
+  // Hide the current tab:
+  collectionOfTabs[currentTab].classList.remove('tab-active');
+  currentTab = currentTab + n;
+
+  // if the last step
+  if (currentTab >= collectionOfTabs.length) {
+    showResults();
+    return false;
+  }
+  saveTheValues();
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+  
+}
+
+function validateForm() {
+  let valid = true;
+  
+  let collectionOfTabs = document.getElementsByClassName("tab");
+  let inputsToValidatePerSteps = collectionOfTabs[currentTab].getElementsByClassName("user-input");
+
+  // A loop that checks every input field in the current tab:
+  for (let i = 0; i < inputsToValidatePerSteps.length; i++) {
+    
+    const errorBox = document.createElement('p');
+    errorBox.className = 'message-error'
+    const currentElement = inputsToValidatePerSteps[i];
+    if (!currentElement.checkValidity()) {
+      
+      if(currentElement.nextSibling.className === "message-error") {
+        currentElement.nextSibling.remove();
+      }
+      currentElement.after(errorBox);
+
+      errorBox.innerHTML = currentElement.validationMessage;
+      currentElement.classList.add('invalid');
+      valid = false;
+    } 
+    else {
+      currentElement.classList.remove('invalid');
+      currentElement.classList.add('valid');
+      if(currentElement.nextSibling.className === "message-error") {
+        currentElement.nextSibling.remove();
+      }
+      
+    }
+    
+  }
+
+  return valid; 
+}
+
+
+function showResults() {
+  const popupContent = document.querySelector('.popup-content');
+  popupContent.innerHTML = "";
+  const resultsHeadline = document.createElement('h2');
+  resultsHeadline.appendChild(document.createTextNode('Thank you for completing our survey!'));
+  popupContent.appendChild(resultsHeadline);
+
+  const resultsTable = document.createElement('table');
+  resultsTable.classList.add('u-full-width');
+  const thead = document.createElement('thead');
+  thead.innerHTML = "<th>Property</th><th>Details</th>";
+
+  const tbody = document.createElement('tbody');
+
+    Object.keys(user).forEach(function(property) {
+      let resultsTableRowHtml = `
+      <tr>
+        <td>${property}</td>
+        <td>${user[property]}</td>
+      </tr>`;
+      tbody.innerHTML += resultsTableRowHtml;
+
+  });
+  resultsTable.appendChild(thead);
+  resultsTable.appendChild(tbody);
+  popupContent.appendChild(resultsTable);
+}
+
+function saveTheValues() {
+  let fields = document.querySelectorAll('.user-input');
+  let userProperties = Object.keys(user);
+  let arrayOfvalues = [];
+  for(let i = 0; i < userProperties.length; i++) {
+   
+    fields.forEach(function(field) {
+
+       if(field.name === userProperties[i]) {
+        
+          if(field.type === "text" || 
+          field.type === "textarea" || 
+          field.type === "email" || 
+          field.type === "select-one" ||
+          field.type === "radio" && field.checked) 
+          {
+            user[userProperties[i]] = field.value;
+          } 
+          else if (field.type === "checkbox" && field.checked && typeof(user[userProperties[i]]) === "object") {
+            arrayOfvalues.push(field.value);
+            user[userProperties[i]] = arrayOfvalues;
+          }
+      }
+    })
+  }
+}
+
+function saveToLocalStorage() {
   localStorage.setItem('userObject', JSON.stringify(user));
 }
 
@@ -118,185 +247,10 @@ function readFromLocalStorage() {
               c.toLowerCase() === field.value.toLowerCase() ? field.checked = true : null;
             })
             
-         
-            
           }
-          
-
       }
-
 
     })
   
   }
 };
-
-
-
-
-
-//from w3s
-
-
-function showTab(n) {
-  // This function will display the specified tab of the form ...
-  var x = document.getElementsByClassName("tab");
-  //x[n].style.display = "block";
-
-  x[n].classList.add('tab-active');
-
-  // x[n].classList.add('tab-active');
-
-  // ... and fix the Previous/Next buttons:
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
-  if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
-  // ... and run a function that displays the correct step indicator:
-  fixStepIndicator(n)
-  //saveTheValues();
-}
-
-function saveTheValues() {
-  let fields = document.querySelectorAll('.user-input');
-  let userProperties = Object.keys(user);
-  let arrayOfvalues = [];
-  for(let i = 0; i < userProperties.length; i++) {
-   
-    fields.forEach(function(field) {
-
-       if(field.name === userProperties[i]) {
-        
-          if(field.type === "text" || 
-          field.type === "textarea" || 
-          field.type === "email" || 
-          field.type === "select-one" ||
-          field.type === "radio" && field.checked) 
-          {
-            user[userProperties[i]] = field.value;
-            //console.log(typeof  user[userProperties[i]]);
-          } 
-          else if (field.type === "checkbox" && field.checked && typeof(user[userProperties[i]]) === "object") {
-            arrayOfvalues.push(field.value);
-            user[userProperties[i]] = arrayOfvalues;
-          }
-          
-
-      }
-
-
-    })
-
-  }
-
-
-}
-
-
-
-function nextPrev(n) {
-  // This function will figure out which tab to display
-  var collectionOfTabs = document.getElementsByClassName("tab");
-  // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
-  // Hide the current tab:
-  //collectionOfTabs[currentTab].style.display = "none";
-  collectionOfTabs[currentTab].classList.remove('tab-active');
-
-
-  saveTheValues();
-  // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
-  // if you have reached the end of the form... :
-  if (currentTab >= collectionOfTabs.length) {
-    //...the form gets submitted:
-    //document.getElementById("surveyForm").submit();
-
-    showResults();
-    return false;
-  }
-  // Otherwise, display the correct tab:
-  showTab(currentTab);
-  
-}
-
-function showResults() {
-  const popupContent = document.querySelector('.popup-content');
-  popupContent.innerHTML = ""
-  const resultsHeadline = document.createElement('h2');
-  resultsHeadline.appendChild(document.createTextNode('Thank you for completing our survey!'))
-  popupContent.appendChild(resultsHeadline);
-
-  const resultsTable = document.createElement('table');
-  resultsTable.classList.add('u-full-width');
-  const thead = document.createElement('thead');
-  thead.innerHTML = "<th>Property</th><th>Details</th>";
-
-  const tbody = document.createElement('tbody');
-
-    Object.keys(user).forEach(function(property) {
-      console.log(property, user[property]);
-
-      let resultsTableRowHtml = `
-      <tr>
-        <td>${property}</td>
-        <td>${user[property]}</td>
-      </tr>`;
-
-      tbody.innerHTML += resultsTableRowHtml;
-
-
-  });
-  resultsTable.appendChild(thead);
-  resultsTable.appendChild(tbody);
-
-  popupContent.appendChild(resultsTable);
-}
-
-function validateForm() {
-  let valid = true;
-  
-  //return valid; //temporary tot est
-
-
-  let collectionOfTabs = document.getElementsByClassName("tab");
-  let inputsToValidatePerSteps = collectionOfTabs[currentTab].getElementsByClassName("user-input");
-
-  // A loop that checks every input field in the current tab:
-  for (let i = 0; i < inputsToValidatePerSteps.length; i++) {
-    
-    const errorBox = document.createElement('p');
-    errorBox.className = 'message-error'
-    const currentElement = inputsToValidatePerSteps[i];
-    if (!currentElement.checkValidity()) {
-      
-      if(currentElement.nextSibling.className === "message-error") {
-        currentElement.nextSibling.remove();
-      }
-      currentElement.after(errorBox);
-
-      errorBox.innerHTML = currentElement.validationMessage;
-      currentElement.classList.add('invalid');
-      valid = false;
-    } 
-    else {
-      currentElement.classList.remove('invalid');
-      if(currentElement.nextSibling.className === "message-error") {
-        currentElement.nextSibling.remove();
-      }
-      
-    }
-    
-  }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid; // return the valid status
-}
